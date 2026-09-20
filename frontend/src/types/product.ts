d4ac66ae9ad22ada5349b69687bcabb8ce362b82
Arda Tatlı <ahmetarda297210@gmail.com>
2026-09-20 16:16:13 +0300

    Connect frontend to product API

---
+++ frontend/src/types/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: number
+  brand: string
+  name: string
+  storage_gb: number
+  ram_gb: number
+  screen_inches: number
+  battery_mah: number
+  main_camera_mp: number
+  has_5g: boolean
+}
